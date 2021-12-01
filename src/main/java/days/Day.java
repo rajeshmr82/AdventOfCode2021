60412dc4c0ae990b33cbf1917418f58b099cc1fc
Rajesh M R <rajesh.mr82@gmail.com>
2021-12-01 13:46:42 +0530

    Day 1

--- src/main/java/days/Day.java
+++ src/main/java/days/Day.java
@@ -7,12 +7,27 @@ import java.time.Duration;
 public abstract class Day {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
-    public String inputFile;
-    public String getInput() {
+    public String inputFilePart1;
+    public String inputFilePart2;
+    public String getInputPart1() {
         try {
-            File f = new File(this.inputFile);
+            File f = new File(this.inputFilePart1);
             if (!f.exists()) {
-                System.out.println(String.format("%s not found", this.inputFile));
+                System.out.printf("%s not found%n", this.inputFilePart1);
+                return null;
+            }
+            Scanner scanner = new Scanner(f);
+            return scanner.useDelimiter("\\Z").next();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public String getInputPart2() {
+        try {
+            File f = new File(this.inputFilePart2);
+            if (!f.exists()) {
+                System.out.printf("%s not found%n", this.inputFilePart2);
                 return null;
             }
             Scanner scanner = new Scanner(f);
@@ -25,19 +40,19 @@ public abstract class Day {
     public abstract void part1();
     public abstract void part2();
     public void run() {
-        System.out.println(String.format("%sRunning %s%s", ANSI_GREEN, this.getClass().getName(), ANSI_RESET));
+        System.out.printf("%sRunning %s%s%n", ANSI_GREEN, this.getClass().getName(), ANSI_RESET);
 
         Instant start1 = Instant.now();
         this.part1();
         Instant end1 = Instant.now();
         long runtimePart1 = Duration.between(start1, end1).toMillis();
-        System.out.println(String.format("Part 1 took %d ms", runtimePart1));
+        System.out.printf("Part 1 took %d ms%n", runtimePart1);
 
         Instant start2 = Instant.now();
         this.part2();
         Instant end2 = Instant.now();
         long runtimePart2 = Duration.between(start2, end2).toMillis();
-        System.out.println(String.format("Part 2 took %d ms", runtimePart2));
-        System.out.println(String.format("\n%s", ANSI_RESET));
+        System.out.printf("Part 2 took %d ms%n", runtimePart2);
+        System.out.printf("\n%s%n", ANSI_RESET);
     }
 }
--- src/main/java/days/Day1/Day1.java
+++ src/main/java/days/Day1/Day1.java
@@ -2,17 +2,52 @@ package days.Day1;
 
 import days.Day;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Day1 extends Day {
     public Day1(){
-        inputFile = "days/Day1/input0.txt";
+        inputFilePart1 = "src/main/java/days/Day1/input1.txt";
+        inputFilePart2 = "src/main/java/days/Day1/input2.txt";
     }
 
     public void part1(){
+        String input= this.getInputPart1();
+        int prev = -1;
+        int largerThanPrev=0;
+        for (String line : input.split("\r\n")) {
+            if(line.isEmpty()) break;
 
+            int num = Integer.parseInt(line);
+            if(prev!=-1 && num>prev){
+                largerThanPrev++;
+            }
+            prev=num;
+        }
+        System.out.printf("measurements are larger than the previous measurement: %d%n", largerThanPrev);
     }
 
-    public void part2(){
+    public void part2() {
+        String input = this.getInputPart2();
+        Queue<Integer> queue = new LinkedList<>();
 
+        int largerThanPrev = 0, sum = 0, prevSum = 0;
+        for (String line : input.split("\r\n")) {
+            int num = Integer.parseInt(line);
+            queue.add(num);
+            sum += num;
+            if (queue.size() == 3) {
+                if (prevSum > 0 && sum > prevSum) {
+                    largerThanPrev++;
+                }
+                prevSum = sum;
+                int oldest = queue.poll();
+                sum -= oldest;
+            }
+        }
+
+
+        System.out.printf("measurements are larger than the previous measurement: %d%n", largerThanPrev);
     }
 
 }
