33f0f997c9dc51dd5d58155b62ea229dfbad6d48
Rajesh M R <rajesh.mr82@gmail.com>
2021-12-23 16:11:12 +0530

    Day 19 - Refactored Point3D for reuse

--- src/main/java/days/Day19.java
+++ src/main/java/days/Day19.java
@@ -7,64 +7,6 @@ import java.util.stream.Collectors;
 
 public class Day19 extends Day {
 
-    static class Point3D{
-        int x;
-        int y;
-        int z;
-
-        public Point3D(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public int getAxis(Axes axis){
-            switch (axis){
-                case X_AXIS:
-                    return x;
-                case Y_AXIS:
-                    return y;
-                case Z_AXIS:
-                    return z;
-                default:
-                    return -1;
-            }
-        }
-
-        public void update(int x,int y,int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("{%d,%d,%d}",x,y,z);
-        }
-
-        @Override
-        public boolean equals(Object o)
-        {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
-            Point3D point = (Point3D) o;
-
-            return x == point.x && y == point.y && z == point.z;
-        }
-
-        @Override
-        public int hashCode()
-        {
-            return this.x + this.y + this.z;
-        }
-
-    }
-
     static class Scanner {
         int id;
         List<Point3D> beacons = new ArrayList<>();
